cbb378f87e9dd023800d0b623e0dd23952df10c7
Avnish <avnish@query.ai>
2022-04-27 16:07:59 +0530

    daptor swagger UI

--- dev-helpers/dev-helper-initializer.js
+++ dev-helpers/dev-helper-initializer.js
@@ -4,7 +4,8 @@ window.onload = function() {
   window["SwaggerUIStandalonePreset"] = window["swagger-ui-standalone-preset"]
   // Build a system
   const ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    // url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "./examples/queryai_adapter_openapi_1.0.0.yaml",
     dom_id: "#swagger-ui",
     presets: [
       SwaggerUIBundle.presets.apis,
